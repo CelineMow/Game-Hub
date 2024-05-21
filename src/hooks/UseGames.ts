012d78806ce396439aee6050ee8fc53d2d3e1492
Celine <celine.pro.morel@gmail.com>
2024-05-21 09:16:56 +0200

    update skeleton

--- src/hooks/UseGames.ts
+++ src/hooks/UseGames.ts
@@ -9,36 +9,43 @@ export interface Platform {
 }
 
 export interface Game {
-    id: number;
-    name: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-  }
-  interface FetchGameResponse {
-    count: number;
-    results: Game[];
-  }
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-
-    const controller = new AbortController();
-
-    apiClient
-      .get<FetchGameResponse>("/games", {signal: controller.signal})
-      .then((res) => setGames(res.data.results))
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message)});
-
-      return () => 
-        controller.abort();
-  }, []);
-
-  return ({games, error});
+  id: number;
+  name: string;
+  background_image: string;
+  parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
-export default useGames
+interface FetchGameResponse {
+  count: number;
+  results: Game[];
+}
+
+const useGames = () => {
+  const [games, setGames] = useState<Game[]>([]);
+  const [error, setError] = useState("");
+  const [isLoading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    setLoading(true);
+    apiClient
+      .get<FetchGameResponse>("/games", { signal: controller.signal })
+      .then((res) => {
+        setGames(res.data.results);
+        setLoading(false);
+      })
+      .catch((err) => {
+        if (err instanceof CanceledError) return;
+        setError(err.message);
+        setLoading(false);
+      });
+
+    return () => controller.abort();
+  }, []);
+
+  return { games, error, isLoading };
+};
+
+export default useGames;
